46158b964847ccf9f44b371f8953170b87af9936
jjh9597 <howie9597@gmail.com>
2024-01-23 18:15:59 +0900

    ss

--- Java_20240122_제어문/src/ex04/Array02.java
+++ Java_20240122_제어문/src/ex04/Array02.java
@@ -28,7 +28,21 @@ public class Array02 {
 			total += arrNum[i];
 		
 		avg =  (double)total/arrNum.length;
-		System.out.println("총점 : " + total + ", 평균" + avg);
+		System.out.print("총점 : " + total + ", 평균" + avg);
+		
+		System.out.println("\n-----------------------------");
+		
+		// 최대값 구하기
+		int max = 0;
+		for(int i=0; i<arrNum.length; i++)
+			if(max<arrNum[i])
+				max = arrNum[i];
+		
+		System.out.print("최대값 : " + max);
+		
+		System.out.println("\n-----------------------------");
+		
+		//정렬
 		
 	}
 
